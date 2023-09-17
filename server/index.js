9eeabf8bf9d73b7db85339194c3aabb8a4f07578
Sreedharan Namboodiri C <89787284+SridharDigital@users.noreply.github.com>
2023-09-17 21:28:07 +0530

    Created post api for creating account for new users. It also includes validating email already exists or not and hashing password before posting

--- server/index.js
+++ server/index.js
@@ -1,14 +1,50 @@
 const express = require("express")
 const bodyParser = require("body-parser")
 const mongoose = require("mongoose")
+const bcrypt = require("bcrypt")
 const jwt = require("jsonwebtoken")
+cors = require("cors")
 const dotenv = require("dotenv")
 dotenv.config()
 
 const app = express()
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.json())
+app.use(cors())
+// app.use(bodyParser.json())
+// app.use(bodyParser.urlencoded({ extended: false }))
+
+const User = mongoose.model("User", {
+  name: String,
+  email: String,
+  mobile: Number,
+  password: String,
+})
+
+app.post("/api/user", async (req, res) => {
+  try {
+    const { name, email, mobile, password } = req.body
+    const existingUser = await User.findOne({ email })
+    if (existingUser) {
+      res.send({
+        status: "FAIL",
+        message: "The email already exists in the database",
+      })
+    } else {
+      const hashedPassword = await bcrypt.hash(password, 10)
+      console.log(hashedPassword)
+      const newUser = { name, email, mobile, password: hashedPassword }
+      await User.create(newUser)
+      res.send({
+        status: "SUCCESS",
+        message: "The Account has been created successfully",
+      })
+    }
+  } catch (error) {
+    console.log(error)
+    res.send(error)
+  }
+})
 
 app.listen(process.env.PORT, async () => {
   try {
