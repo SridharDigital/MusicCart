04058bc2ff5cf065d30bca6e2439e4aa9734a600
Sreedharan Namboodiri C <89787284+SridharDigital@users.noreply.github.com>
2023-09-17 15:47:29 +0530

    created Order success page

--- client/src/pages/orderSuccess/OrderSuccess.jsx
+++ client/src/pages/orderSuccess/OrderSuccess.jsx
@@ -1,10 +1,34 @@
 import "./orderSuccess.css"
+import successImage from "../../assets/successImage.png"
 import Navbar from "../../components/navbar/Navbar"
+import Logo from "../../components/logo/Logo"
+import ContentWrapper from "../../components/contentWrapper/ContentWrapper"
 
 const OrderSuccess = () => {
   return (
     <>
       <Navbar displaySearchBarinMobile={false} />
+      <ContentWrapper customClasses={"order-success-center"}>
+        <div className="logo-start">
+          <Logo />
+        </div>
+        <div className="order-success-container">
+          <img
+            src={successImage}
+            alt="success image"
+            className="success-image"
+          />
+          <h1 className="order-success-heading">
+            Order is placed successfully!
+          </h1>
+          <p className="order-success-text">
+            You will be receiving a confirmation email with order details
+          </p>
+          <button className="primary-button go-to-home-page-button">
+            Go back to Home page
+          </button>
+        </div>
+      </ContentWrapper>
     </>
   )
 }
