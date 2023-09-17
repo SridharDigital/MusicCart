85e8cd604bcbf6c7c047636bd3209088f7934bf3
Sreedharan Namboodiri C <89787284+SridharDigital@users.noreply.github.com>
2023-09-17 22:46:02 +0530

    Created POST API for validating login and also validating mobile number already exists or not while creating account

--- server/index.js
+++ server/index.js
@@ -1,5 +1,4 @@
 const express = require("express")
-const bodyParser = require("body-parser")
 const mongoose = require("mongoose")
 const bcrypt = require("bcrypt")
 const jwt = require("jsonwebtoken")
@@ -11,35 +10,75 @@ const app = express()
 
 app.use(express.json())
 app.use(cors())
-// app.use(bodyParser.json())
-// app.use(bodyParser.urlencoded({ extended: false }))
 
 const User = mongoose.model("User", {
   name: String,
   email: String,
-  mobile: Number,
+  mobile: String,
   password: String,
 })
 
-app.post("/api/user", async (req, res) => {
+app.post("/api/login", async (req, res) => {
+  try {
+    const { emailOrMobile, password } = req.body
+    const existingUser = await User.findOne({
+      $or: [{ email: emailOrMobile.toLowerCase() }, { mobile: emailOrMobile }],
+    })
+    if (existingUser) {
+      const existingHashedPassword = existingUser.password
+      const isPasswordMatch = await bcrypt.compare(
+        password,
+        existingHashedPassword
+      )
+      if (isPasswordMatch) {
+        res.send({
+          status: "SUCCESS",
+          message: "Login Successful",
+        })
+      } else throw "Incorrect password"
+      console.log("The password match status is", isPasswordMatch)
+    } else {
+      console.log("Error thrown")
+      throw "User not found"
+    }
+    console.log({ existingUser })
+  } catch (error) {
+    console.log({ error })
+    res.send({
+      status: "FAIL",
+      message: error,
+    })
+  }
+})
+
+app.post("/api/signup", async (req, res) => {
   try {
     const { name, email, mobile, password } = req.body
-    const existingUser = await User.findOne({ email })
-    if (existingUser) {
-      res.send({
-        status: "FAIL",
-        message: "The email already exists in the database",
-      })
-    } else {
-      const hashedPassword = await bcrypt.hash(password, 10)
-      console.log(hashedPassword)
-      const newUser = { name, email, mobile, password: hashedPassword }
-      await User.create(newUser)
-      res.send({
-        status: "SUCCESS",
-        message: "The Account has been created successfully",
-      })
+    const doEmailAlreadyExists = await User.findOne({
+      email: email.toLowerCase(),
+    })
+    if (doEmailAlreadyExists) {
+      throw "Email already exists"
+      return
     }
+    const doMobileAlreadyExists = await User.findOne({ mobile })
+    if (doMobileAlreadyExists) {
+      throw "Mobile number already exists"
+      return
+    }
+    const hashedPassword = await bcrypt.hash(password, 10)
+    console.log(hashedPassword)
+    const newUser = {
+      name,
+      email: email.toLowerCase(),
+      mobile,
+      password: hashedPassword,
+    }
+    await User.create(newUser)
+    res.send({
+      status: "SUCCESS",
+      message: "The Account has been created successfully",
+    })
   } catch (error) {
     console.log(error)
     res.send(error)
