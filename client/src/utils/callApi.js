e053f4bda6c0391c10271c663bb51156adb69e8e
Sreedharan Namboodiri C <89787284+SridharDigital@users.noreply.github.com>
2023-09-22 16:18:11 +0530

    fixed error in sending params in APIs

--- client/src/utils/callApi.js
+++ client/src/utils/callApi.js
@@ -5,11 +5,10 @@ const BASE_URL = "http://localhost:4100/api"
 export const callApi = async (method, url, params) => {
   try {
     if (method === "GET") {
-      const { data } = await axios.get(BASE_URL + url, params)
-      console.log(data)
+      const { data } = await axios.get(BASE_URL + url, { params })
       return data
     } else if (method === "POST") {
-      const { data } = await axios.post(BASE_URL + url, params)
+      const { data } = await axios.post(BASE_URL + url, { params })
       return data
     }
   } catch (error) {
