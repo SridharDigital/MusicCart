bc578b65e5f7b508e0e174a9d8e4d99b61d906c7
Sreedharan Namboodiri C <89787284+SridharDigital@users.noreply.github.com>
2023-09-29 14:26:58 +0530

    Setup redux-toolkit and react-redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 
+import { store } from "./store/store"
+import { Provider } from "react-redux"
+
 import "./App.css"
 import ProtectedRoute from "./utils/ProtectedRoute"
-
 import Navbar from "./components/navbar/Navbar"
 import MobileFooterNavbar from "./components/mobileFooterNavbar/MobileFooterNavbar"
 import Home from "./pages/home/Home"
@@ -16,27 +18,29 @@ import NotFound from "./pages/notFound/NotFound"
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route
-          path="/checkout"
-          element={
-            <ProtectedRoute>
-              <Checkout />
-            </ProtectedRoute>
-          }
-        />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/" element={<Home />} />
-        <Route path="/product/:id" element={<ProductDetails />} />
-        <Route path="/order-success" element={<OrderSuccess />} />
-        <Route path="/*" element={<NotFound />} />
-      </Routes>
-      <MobileFooterNavbar />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route
+            path="/checkout"
+            element={
+              <ProtectedRoute>
+                <Checkout />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/product/:id" element={<ProductDetails />} />
+          <Route path="/order-success" element={<OrderSuccess />} />
+          <Route path="/*" element={<NotFound />} />
+        </Routes>
+        <MobileFooterNavbar />
+      </BrowserRouter>
+    </Provider>
   )
 }
 
