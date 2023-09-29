1fd6357df6d7b914e5ce2b82839a8f1d93388457
Sreedharan Namboodiri C <89787284+SridharDigital@users.noreply.github.com>
2023-09-29 22:43:56 +0530

    added cartItem total in redux store

--- client/src/store/userSlice.js
+++ client/src/store/userSlice.js
@@ -4,7 +4,8 @@ import { validateUserLoggedin } from "../utils/validateUserLoggedin"
 
 const initialState = {
   isUserLoggedIn: validateUserLoggedin(),
-  userId: "",
+  userId: Cookies.get("userId"),
+  cartToatal: 0,
 }
 
 export const userSlice = createSlice({
@@ -12,9 +13,13 @@ export const userSlice = createSlice({
   initialState,
   reducers: {
     loginUser: (state, action) => {
-      console.log("The action payload is ", action.payload.user.id)
-      console.log("loginUser action is executed")
+      // console.log("The action payload is ", action.payload.user.id)
+      // console.log("loginUser action is executed")
       Cookies.set("jwt_token", action.payload.jwtToken, {
+        expires: 30,
+        path: "/",
+      })
+      Cookies.set("userId", action.payload.user.id, {
         expires: 30,
         path: "/",
       })
@@ -22,16 +27,18 @@ export const userSlice = createSlice({
       state.isUserLoggedIn = validateUserLoggedin()
     },
     logoutUser: (state) => {
-      Cookies.remove("jwt_Token", {
-        path: "/",
-      })
+      Cookies.remove("jwt_token", { path: "/" })
+      Cookies.remove("userId", { path: "/" })
       console.log("logoutUser action is executed")
       state.isUserLoggedIn = validateUserLoggedin()
+    },
+    updateCartTotal: (state, action) => {
+      state.cartToatal = action.payload
     },
   },
 })
 
 // Action creators are generated for each case reducer function
-export const { loginUser, logoutUser } = userSlice.actions
+export const { loginUser, logoutUser, updateCartTotal } = userSlice.actions
 
 export default userSlice.reducer
